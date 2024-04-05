3c8e19db6823604ecfd93a4c678232ea44d70ec7
SADHU-DHANUNJAYA <121181478+SADHU-DHANUNJAYA@users.noreply.github.com>
2024-04-05 10:19:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,3 @@
-terraform {
-  required_version = ">= 0.12.24"
-#  backend "s3" {
-#    bucket = "cloudquickpocsbackendtf"
-#    key="quickpocsbackendtf.tfstates"
-#    region = "us-east-1"
-#  }
-}
-provider "aws" {
-#  region = "us-east-1"
-  region="eu-central-1"
-}
 
 resource "aws_s3_bucket" "example" {
  # bucket = var.config["bucket_name"]
